393cfaa3f2c33fe1ead87456ff1fc19011b2e867
Suhas kale <45892634+suhasvkale@users.noreply.github.com>
2023-08-09 13:05:24 +0000

    Lecture 2 basic notes

---
+++ basic/02.js
@@ -0,0 +1,11 @@
+"use strict";// treat all code as newer version
+let age = 43 ;
+let firstName = "Suhas";
+let boolean = true;
+let a=null;
+let b;
+console.log(typeof b);
+console.log(typeof(a));
+console.log(age);
+console.log(firstName);
+console.log(boolean);
--- basic/abc.js
+++ basic/abc.js
@@ -1,2 +1,12 @@
-console.log("Suhas");
-console.log("Hariom");
+const accountId =123345;
+let firstName ="Suhas";
+let lastName = "Kale";
+var email = "suhasvkale@gmail.com";
+let city ;
+
+// accountId = 23465;// not allowed
+firstName = "Ramesh";
+lastName = "Khate";
+email = "s@gamil.com";
+console.log(accountId);
+console.table([firstName,lastName,email,city]);
